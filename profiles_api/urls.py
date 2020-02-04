fe65812723bb90328afa5f9ed8ad79823c6dc7fb
AswiniKumaran <aswinikumaran94@gmail.com>
2020-02-04 10:17:45 +0530

    added viewsets

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from profiles_api.views import HelloApiView
+from django.urls import path,include
+from profiles_api.views import HelloApiView,HelloAPiViewSet
+from rest_framework.routers import DefaultRouter
 
+router=DefaultRouter()
+router.register('hello_api_viewset',HelloAPiViewSet,base_name='hello_api_viewset')
 urlpatterns=[
-    path('hello_api_view/',HelloApiView.as_view())
+    path('hello_api_view/',HelloApiView.as_view()),
+    path('',include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from profiles_api.serializers import HelloSerializer
 from rest_framework import status
+from rest_framework.viewsets import ViewSet
 
 class HelloApiView(APIView):
     serializer_class=HelloSerializer
@@ -33,4 +34,33 @@ class HelloApiView(APIView):
     def patch(self,request):
         return Response({'method':'PATCH'})
     def delete(self,request):
+        return Response({'method':'DELETE'})
+
+class HelloAPiViewSet(ViewSet):
+    serializer_class=HelloSerializer
+    def list(self,request):
+        messge=[
+            'automatically mapped to urls through routers',
+            'involves less code change',
+            'contains methods create,retreive,update,delete,partial_update'
+        ]
+        return Response({'messge':messge})
+    def create(self,request):
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}'
+            return Response({'messge':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    def retreive(self,request):
+        return Response({'method':'GET'})
+    def update(self,request,pk=None):
+        return Response({'method':'PUT'})
+    def partial_update(self,request,pk=None):
+        return Response({'method':'PATCH'})
+    def delete(self,request,pk=None):
         return Response({'method':'DELETE'})
